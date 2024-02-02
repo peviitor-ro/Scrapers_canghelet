a61645b8c33c540422fb6399faa8840ccb62127f
Cristina Anghelet <cristina.anghelet@gmail.com>
2024-02-02 17:48:16 +0200

    update urban scraper

--- sites/urban.py
+++ sites/urban.py
@@ -5,11 +5,14 @@
 from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
-import uuid
 from bs4 import BeautifulSoup
 
 
 def get_jobs():
+    """
+    ... this func() makes requests
+     and collects data from Urban API.
+     """
 
     list_jobs = []
 
@@ -22,18 +25,16 @@ def get_jobs():
         soup = BeautifulSoup(response.text, 'lxml')
         jobs = soup.find_all('div', class_='hp-listing__content')
 
-
         if len(jobs) > 0:
             for job in jobs:
                 link = job.find('a')['href']
                 title = job.find('a').text.strip()
-                list_city = ['Timisoara', 'Bucuresti']
+                list_cities = ['Timisoara', 'Bucuresti']
                 try:
                     location = job.find('div', class_='hp-listing__attribute hp-listing__attribute--location').text.split(',')[0].strip().split('/ ')
                 except:
                     location = 'Timisoara'
                     job_type = 'remote'
-
 
                 if 'Remote' in location:
                     location = 'Timisoara'
@@ -45,7 +46,6 @@ def get_jobs():
                     location = 'Cluj-Napoca'
 
                 list_jobs.append({
-                    "id": str(uuid.uuid4()),
                     "job_title": title,
                     "job_link": link,
                     "company": "UrbanSocializing",
