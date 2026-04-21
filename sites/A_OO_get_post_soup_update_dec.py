32ea13f9b1e22f13964f03df8c3f751a8f1f2d4c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-04-21 11:48:40 +0300

    chore: clean up empty code change sections in the changes log

--- sites/A_OO_get_post_soup_update_dec.py
+++ sites/A_OO_get_post_soup_update_dec.py
@@ -1,7 +1,15 @@
+#
+#
+#
+# This Decorator will avoid duplicate for me!
+# Respect DRY
+# This decorator have: default headers and soup and update data on peviitor.ro!
+#
 import requests
-import os
+#
+import os  # I do not have API KEY
+#
 import json
-import time
 
 
 DEFAULT_HEADERS = {
@@ -22,41 +30,25 @@ def update_peviitor_api(original_function):
         company_name, data_list = args
         #
         API_KEY = os.environ.get('API_KEY')
-        CLEAN_URL = 'https://api.peviitor.ro/v4/clean/'
 
-        clean_header = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'apikey': API_KEY
-
-            }
-
-
-        # clean_request = requests.post(CLEAN_URL, headers=clean_header, data={'company': company_name})
-
-        time.sleep(0.2)
         token = get_token()
-
         post_header = {
             'Content-Type': 'application/json',
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
         }
+        validator_endpoint = 'https://api.laurentiumarian.ro/jobs/add/'
 
-        validator_endpoint = 'https://api.peviitor.ro/v5/add/'
-        res = requests.post(validator_endpoint, json=data_list, headers=post_header)
-        # post_request_to_server = requests.post('https://api.peviitor.ro/v4/update/', headers=post_header, data=json.dumps(data_list))
+        res = requests.post(validator_endpoint,
+                            json=data_list, headers=post_header)
         print(json.dumps(data_list, indent=4))
-        print(res.status_code)
 
-        # post_request_to_header = requests.post('https://api.peviitor.ro/v5/add/', headers=post_header, json=data_list)
-        # print(json.dumps(data_list, indent=4))
-
-
-         # don't delete this lines if you want to see the graph on scraper's page
-        #file = company_name.lower() + '.py'
-        #data = {'data': len(data_list)}
-        #dataset_url = f'https://dev.laurentiumarian.ro/dataset/Scrapers_canghelet/{file}/'
-        #requests.post(dataset_url, json=data)
-        ########################################################
+        if res.status_code == 200:
+            print(
+                f"Data for {company_name} updated successfully on Peviitor API!")
+        else:
+            print(
+                f"Failed to update data for {company_name} on Peviitor API. Status code: {res.status_code}, Response: {res.text}")
 
         return original_function(*args, **kwargs)
 
@@ -64,17 +56,14 @@ def update_peviitor_api(original_function):
 
 
 def get_token():
-    #token_endpoint = 'https://api.peviitor.ro/v5/get_token/'
+    # token_endpoint = 'https://api.peviitor.ro/v5/get_token/'
     token_endpoint = 'https://api.laurentiumarian.ro/get_token'
+    email = os.environ.get('API_KEY')
+
     token = requests.post(token_endpoint, json={
-        "email": "cristina.anghelet@gmail.com"
-    },  headers = {
-    "Content-Type": "application/json",
-})
+        "email": email
+    }, headers={
+        "Content-Type": "application/json",
+    })
 
     return token.json()['access']
-
-
-
-
-
