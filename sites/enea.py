2c07e43629b590223e5d3b0de29753cd5555dd24
lalalaurentiu <67306273+lalalaurentiu@users.noreply.github.com>
2026-06-05 14:07:02 +0000

    chore: update scraper outputs

--- sites/enea.py
+++ sites/enea.py
@@ -1,46 +1,43 @@
 # company: ENEA
-# API: https://careers.enea.com/jobs?country=Romania&query=
+# API: https://careers.enea.com/jobs.json
 
 
 from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
-from bs4 import BeautifulSoup
 from _county import get_county, translate_city
 
 
 def get_all_jobs():
     """
     ... this func() makes requests
-    and collects data from ENEA API.
+    and collects data from ENEA JSON feed.
     """
 
     list_of_jobs = []
     response = requests.get(
-        'https://careers.enea.com/jobs?country=Romania&query=', headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text, 'lxml')
+        'https://careers.enea.com/jobs.json', headers=DEFAULT_HEADERS)
+    data = response.json()
 
-    jobs = soup.find('ul', id='jobs_list_container').find_all('li', recursive=False)
-
-    for job in jobs:
-        link_tag = job.find('a', href=True)
-        if not link_tag or 'jobs/' not in link_tag['href']:
+    for item in data.get('items', []):
+        job = item.get('_jobposting', {})
+        locations = job.get('jobLocation', [])
+        if not locations:
             continue
 
-        title = link_tag.get_text(strip=True)
-        details = job.find('div', class_='mt-1 text-md')
-        if not details:
+        address = locations[0].get('address', {})
+        if address.get('addressCountry') != 'RO':
             continue
 
-        details_text = ' '.join(details.stripped_strings)
-        if 'Bucharest, Romania' not in details_text:
+        city_raw = address.get('addressLocality', '')
+        if not city_raw:
             continue
 
-        city = translate_city('Bucharest')
+        city = translate_city(city_raw)
         county = get_county(city)
         list_of_jobs.append({
-            "job_title": title,
-            "job_link": link_tag['href'],
+            "job_title": job.get('title', item.get('title', '')),
+            "job_link": item.get('url', ''),
             "company": "ENEA",
             "country": "Romania",
             "city": city,
